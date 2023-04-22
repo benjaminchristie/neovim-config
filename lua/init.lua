784bf1034284994eb70a515449690927a07db4ad
Benjamin Christie <benc00@vt.edu>
2023-04-22 12:57:37 -0400

    refactored surround

--- lua/init.lua
+++ lua/init.lua
@@ -1,6 +1,5 @@
 require("impatient")
 require("colors")
--- require("plugins")
 require("options")
 require("config")
 require("harpoon").setup({})
@@ -15,26 +14,7 @@ require('colorizer').setup({}, { css = true; })
 require('easyread').setup({})
 require('mini.pairs').setup()
 require("modules/statuswinbar")
-require("nvim-surround").setup()
-require("nvim-surround").buffer_setup {
-    surrounds = {
-	["c"] = {
-	    add = function()
-		local cmd = require("nvim-surround.config").get_input "Command: "
-		return { { "\\" .. cmd .. "{" }, { "}" } }
-	    end,
-	},
-	["e"] = {
-	    add = function()
-		local env = require("nvim-surround.config").get_input "Environment: "
-		return { { "\\begin{" .. env .. "}" }, { "\\end{" .. env .. "}" } }
-	    end,
-	},
-    },
-}
--- for some reason, some LSP servers do not attach to files properly if loaded asynchronously 
-
+require("modules/surround")
 require("modules/lsp-init")
 require("modules/term")
 require("modules/starter")
--- -- require('dashboard').setup()
