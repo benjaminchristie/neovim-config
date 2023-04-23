017a8e390acd4accf50c68d116d49ba005738f7c
Benjamin Christie <benc00@vt.edu>
2023-04-23 13:34:16 -0400

    made it prettier

--- lua/modules/starter.lua
+++ lua/modules/starter.lua
@@ -8,14 +8,14 @@ local telescope_items = function()
   return function()
     return {
       {action = 'Telescope find_files',      name = 'Workspace files',           section = 'Telescope'},
-      {action = cached_repos,                name = 'Projects',           section = 'Telescope'},
-      {action = 'Telescope command_history', name = 'Command history', section = 'Telescope'},
-      {action = 'Telescope help_tags',       name = 'Help tags',       section = 'Telescope'},
-      {action = 'Telescope live_grep',       name = 'Live grep',       section = 'Telescope'},
-      {action = 'Telescope oldfiles',        name = 'Recent files',       section = 'Telescope'},
-      {action = 'tab G',                     name = 'Status',        section = 'Fugitive'},
-      {action = 'Git difftool -y',                     name = 'Diff tool',        section = 'Fugitive'},
-      {action = 'Git mergetool -y',                     name = 'Merge tool',        section = 'Fugitive'},
+      {action = cached_repos,                name = 'Projects',                  section = 'Telescope'},
+      {action = 'Telescope command_history', name = 'Command history',           section = 'Telescope'},
+      {action = 'Telescope help_tags',       name = 'Help tags',                 section = 'Telescope'},
+      {action = 'Telescope live_grep',       name = 'Live grep',                 section = 'Telescope'},
+      {action = 'Telescope oldfiles',        name = 'Recent files',              section = 'Telescope'},
+      {action = 'tab G',                     name = 'Status',                    section = 'Fugitive'},
+      {action = 'Git difftool -y',           name = 'Diff tool',                 section = 'Fugitive'},
+      {action = 'Git mergetool -y',          name = 'Merge tool',                section = 'Fugitive'},
     }
   end
 end
@@ -32,6 +32,6 @@ starter.setup({
       starter.gen_hook.aligning("center", "center")
       -- starter.gen_hook.padding(3, 2),
     },
-    footer = ""
+    footer = "",
 })
 
