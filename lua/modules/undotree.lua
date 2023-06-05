12bbba133e17396d2ec486486208a10f92fdd042
Benjamin Christie <benc00@vt.edu>
2023-06-04 23:17:24 -0400

    undotree updated

--- lua/modules/undotree.lua
+++ lua/modules/undotree.lua
@@ -2,5 +2,6 @@ vim.g.undotree_DiffpanelHeight = 17
 vim.g.undotree_WindowLayout = 2
 vim.g.undotree_SetFocusWhenToggle = 1
 vim.g.undotree_ShortIndicators = 1
+vim.g.undotree_DiffCommand = "git diff -p"
 
 vim.keymap.set('n', "U", vim.cmd.UndotreeToggle)
