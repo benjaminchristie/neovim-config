6843346504cd3d04eb18beb8ddb6e323bdc7a832
Benjamin Christie <benc00@vt.edu>
2023-08-07 18:54:00 -0400

    Feat: add inlay hints

--- lua/modules/lsp-init.lua
+++ lua/modules/lsp-init.lua
@@ -1,7 +1,6 @@
 -- Setup lspconfig.
 -- neodev must be called before lspconfig
 require("neodev").setup({
-    -- add any options here, or leave empty to use the default settings
     library = { plugins = { "nvim-dap-ui" }, types = true },
 })
 local lspconfig = require("lspconfig")
@@ -22,18 +21,22 @@ vim.lsp.handlers["textDocument/signatureHelp"] =
             border = "single"
         }
     )
--- Mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-
-
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-    -- Enable completion triggered by <c-x><c-o>
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-    -- client.server_capabilities.semanticTokensProvider = nil
+    vim.api.nvim_create_augroup("lsp_augroup", { clear = true })
+    vim.api.nvim_create_autocmd("InsertEnter", {
+        buffer = bufnr,
+        callback = function() vim.lsp.inlay_hint(bufnr, true) end,
+        group = "lsp_augroup",
+    })
+    vim.api.nvim_create_autocmd("InsertLeave", {
+        buffer = bufnr,
+        callback = function() vim.lsp.inlay_hint(bufnr, false) end,
+        group = "lsp_augroup",
+    })
 end
-
 local lsp_flags = {
     -- This is the default in Nvim 0.7+
     debounce_text_changes = 150,
@@ -50,29 +53,6 @@ lspconfig.pyright.setup {
     capabilities = capabilities,
     autostart = true,
 }
--- lspconfig.pylsp.setup{
---     on_attach = function(client)
---       client.resolved_capabilities.document_formatting = false
---       client.resolved_capabilities.document_range_formatting = false
---       client.server_capabilities.completionProvider = false
---     end,
---     flags = lsp_flags,
---     capabilities = capabilities, -- i only want constructive feedback from pylsp
---     settings = {
---         pylsp = {
---             plugins = {
---                 pycodestyle = {
---                     ignore = {
---                        'W391', 'E231', 'E203', 'E731',
---                        'E126', 'E131', 'E128', 'E101',
---                        'W191', 'E502', 'E226'
---                     },
---                     maxLineLength = 120
---                 }
---             }
---         }
---     }
--- }
 lspconfig['tsserver'].setup {
     on_attach = on_attach,
     flags = lsp_flags,
@@ -108,21 +88,17 @@ lspconfig.lua_ls.setup {
                 callSnippet = "Replace"
             },
             runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
                 version = 'LuaJIT',
             },
             diagnostics = {
-                -- Get the language server to recognize the `vim` global
                 globals = { 'vim' },
                 undefined_global = false,
                 missing_parameters = false,
             },
             workspace = {
-                -- Make the server aware of Neovim runtime files
                 library = vim.api.nvim_get_runtime_file("", true),
                 checkThirdParty = false,
             },
-            -- Do not send telemetry data containing a randomized but unique identifier
             telemetry = {
                 enable = false,
             },
@@ -211,7 +187,7 @@ lspconfig['clangd'].setup({
         "--pretty",
         "--background-index",
         "--clang-tidy",
-        "--clang-tidy-checks='"..table.concat(clang_tidy_checks, ",") .. "'",
+        "--clang-tidy-checks='" .. table.concat(clang_tidy_checks, ",") .. "'",
         "--cross-file-rename",
         "--header-insertion=iwyu",
         "--suggest-missing-includes",
