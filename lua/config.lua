19debf5b88b6bedf2a265dfd1bac15ae82e10b65
Benjamin Christie <benc00@vt.edu>
2023-08-31 23:35:30 -0400

    fix: removed erroneous module includes

--- lua/config.lua
+++ lua/config.lua
@@ -7,8 +7,6 @@ require("modules/pairs-init")
 require("modules/statuswinbar")
 require("modules/surround")
 require("modules/term")
-require("modules/treesitter-init")
-require("modules/undotree")
 
 require("config_autocmds")
 require("config_functions")
