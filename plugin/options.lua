c26168ae555f562553e47a7d0bfd0bd8ad27a9d7
Benjamin Christie <benc00@vt.edu>
2023-09-10 19:59:57 -0400

    fix: formatoptions

--- plugin/options.lua
+++ plugin/options.lua
@@ -27,6 +27,7 @@ vim.o.foldcolumn = '0' -- '0' is not bad
 vim.o.foldenable = true
 vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
+vim.o.formatoptions = "jlqn"
 vim.o.hlsearch = true
 vim.o.ignorecase = true
 vim.o.incsearch = true
