0ac1cc16d9ef7e29a3abc1009e62f2125c0fd7d2
Benjamin Christie <benc00@vt.edu>
2023-09-11 23:09:48 -0400

    feat(init.lua): added vscode flag

--- init.lua
+++ init.lua
@@ -1,4 +1,21 @@
 vim.loader.enable()
+if vim.g.vscode ~= nil then
+    vim.o.hlsearch = true
+    vim.o.incsearch = true
+    vim.keymap.set('v', "J", ":m '>+1<CR>gv=gv")
+    vim.keymap.set('v', "K", ":m '<-2<CR>gv=gv")
+    vim.keymap.set('n', 'n', 'nzz')
+    vim.keymap.set('n', 'N', 'Nzz')
+    vim.keymap.set('n', '<C-d>', '<C-d>zz')
+    vim.keymap.set('n', '<C-u>', '<C-u>zz')
+    vim.keymap.set('n', '}', '}zz')
+    vim.keymap.set('n', '{', '{zz')
+    vim.keymap.set('n', '<Up>', '<C-b>')
+    vim.keymap.set('n', '<Down>', '<C-f>')
+    vim.keymap.set('n', '<Left>', 'gT')
+    vim.keymap.set('n', '<Right>', 'gt')
+    vim.o.loadplugins = false
+end
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
