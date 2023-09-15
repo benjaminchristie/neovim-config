ab3cb47f1e7399b5d9768f9f7b6d77340a0209d3
Benjamin Christie <benc00@vt.edu>
2023-09-14 23:52:51 -0400

    fix(autocmds): better formatting using `event`

--- plugin/autocmds.lua
+++ plugin/autocmds.lua
@@ -18,94 +18,130 @@ local buf_keymap = require("custom-utils").buf_keymap
 local autocmd = vim.api.nvim_create_autocmd
 
 local function create_skeleton(ext)
-    return autocmd({ "BufNewFile" }, {
-        pattern = "*." .. ext,
-        group = augroup("skeletons-" .. ext),
-        command = "0r " .. vim.fn.stdpath("config") .. "/skeletons/skeleton." .. ext
-    })
+	return autocmd({ "BufNewFile" }, {
+		pattern = "*." .. ext,
+		group = augroup("skeletons-" .. ext),
+		command = "0r " .. vim.fn.stdpath("config") .. "/skeletons/skeleton." .. ext
+	})
 end
 
 local function filetype_detection(pattern, desired_filetype)
-    return autocmd({ "BufRead", "BufNewFile" }, {
-        group = augroup("filetype-detection-for-" .. desired_filetype),
-        pattern = pattern,
-        callback = function()
-            vim.opt_local.filetype = desired_filetype
-        end,
-    })
+	return autocmd({ "BufRead", "BufNewFile" }, {
+		group = augroup("filetype-detection-for-" .. desired_filetype),
+		pattern = pattern,
+		callback = function(event)
+			vim.bo[event.buf].filetype = desired_filetype
+		end,
+	})
 end
 
 autocmd({ "BufEnter" }, {
-    group = augroup("FormatGroup"),
-    pattern = "*",
-    callback = function()
-        if string.find(vim.bo.filetype, "dap-") then
-            return
-        end
-        vim.opt.formatoptions:remove({ 'c', 'r', 'o' })
-        vim.opt_local.formatoptions:remove({ 'c', 'r', 'o' })
-        if vim.o.filetype ~= "oil" and vim.o.filetype ~= "starter" and vim.o.filetype ~= "lazy" then
-            if not require("custom-functions").zen_enabled() then
-                vim.wo[0][0].number = true
-                vim.wo[0][0].relativenumber = true
-            end
-        end
-    end
+	group = augroup("FormatGroup"),
+	pattern = "*",
+	callback = function(event)
+		if string.find(vim.bo[event.buf].filetype, "dap-") then
+			return
+		end
+		vim.schedule(function()
+			vim.opt.formatoptions:remove({ 'c', 'r', 'o' })
+			vim.opt_local.formatoptions:remove({ 'c', 'r', 'o' })
+			if vim.bo[event.buf].filetype ~= "oil" and
+				vim.bo[event.buf].filetype ~= "starter" and
+				vim.bo[event.buf].filetype ~= "lazy" then
+				if not require("custom-functions").zen_enabled() then
+					vim.wo[0][0].number = true
+					vim.wo[0][0].relativenumber = true
+				end
+			end
+		end)
+	end
+})
+
+autocmd({ "BufHidden" }, {
+	group = augroup("DeleteHiddenNoNameBuffers"),
+	callback = function(event)
+		if event.file == "" and vim.bo[event.buf].buftype == "" and not vim.bo[event.buf].modified then
+			vim.schedule(function()
+				pcall(vim.api.nvim_buf_delete, event.buf, {})
+			end)
+		end
+	end
 })
 
 autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
-    group = augroup("checktime-autoread"),
-    pattern = "*",
-    command = "checktime"
+	group = augroup("checktime-autoread"),
+	pattern = "*",
+	command = "checktime"
 })
 
 autocmd("TextYankPost", {
-    group = augroup("highlight-yank"),
-    callback = function()
-        vim.highlight.on_yank({ higroup = "HighlightUndo" })
-    end,
+	group = augroup("highlight-yank"),
+	callback = function()
+		vim.highlight.on_yank({ higroup = "HighlightUndo" })
+	end,
 })
 
+
+autocmd({"RecordingEnter"}, {
+	group = augroup("cmdheight0recenter"),
+	callback = function()
+		if vim.o.cmdheight == 0 then
+			vim.o.cmdheight = 1
+			-- only create this autocmd if cmdheight == 0 to begin with 
+			autocmd({"RecordingLeave"}, {
+				group = augroup("cmdheight0recexit"),
+				callback = function()
+					if vim.o.cmdheight == 1 then
+						vim.o.cmdheight = 0
+					end
+					return true
+				end,
+			})
+		end
+	end,
+})
+
+
 autocmd("User", {
-    pattern = { "MiniStarterOpened" },
-    callback = function()
-        buf_keymap("n", "<tab>",
-            ('<Cmd>lua %s<CR>'):format([[require("mini.starter").update_current_item('next')]])
-        )
-        buf_keymap("n", '<s-tab>',
-            ('<Cmd>lua %s<CR>'):format([[require("mini.starter").update_current_item('prev')]])
-        )
-    end
+	pattern = { "MiniStarterOpened" },
+	callback = function()
+		buf_keymap("n", "<tab>",
+			('<Cmd>lua %s<CR>'):format([[require("mini.starter").update_current_item('next')]])
+		)
+		buf_keymap("n", '<s-tab>',
+			('<Cmd>lua %s<CR>'):format([[require("mini.starter").update_current_item('prev')]])
+		)
+	end
 }
 )
 
 
 autocmd({ "FileType" }, {
-    pattern = { "oil", "starter", "lazy" },
-    group = augroup("number-formatting"),
-    callback = function()
-        vim.wo[0][0].number = false
-        vim.wo[0][0].relativenumber = false
-    end
+	pattern = { "oil", "starter", "lazy" },
+	group = augroup("number-formatting"),
+	callback = function()
+		vim.wo[0][0].number = false
+		vim.wo[0][0].relativenumber = false
+	end
 })
 
-autocmd({"TermClose"}, {
-    pattern = "term://*",
-    group = augroup("AutoTermClose"),
-    callback = function ()
-        if vim.v.event_status == 0 then
-            vim.api.nvim_buf_delete(0, {unload = true})
-        end
-    end
+autocmd({ "TermClose" }, {
+	pattern = "term://*",
+	group = augroup("AutoTermClose"),
+	callback = function()
+		if vim.v.event_status == 0 then
+			vim.schedule(function() vim.api.nvim_buf_delete(0, { unload = true }) end)
+		end
+	end
 })
 autocmd({ "TermOpen" }, {
-    pattern = "term://*",
-    group = augroup("AutoTermMappings"),
-    callback = function()
-        buf_keymap('t', '<esc>', [[<C-\><C-n>]], { noremap = true })
-        vim.opt_local.number = false
-        vim.opt_local.relativenumber = false
-    end
+	pattern = "term://*",
+	group = augroup("AutoTermMappings"),
+	callback = function()
+		buf_keymap('t', '<esc>', [[<C-\><C-n>]], { noremap = true })
+		vim.opt_local.number = false
+		vim.opt_local.relativenumber = false
+	end
 })
 
 filetype_detection({ "*.launch", "*.urdf", "*.xacro", "*.xml" }, "html")
