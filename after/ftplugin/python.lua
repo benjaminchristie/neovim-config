a7c4f1440096a069f16bd2a15f3adc7b50ee6d63
Benjamin Christie <benc00@vt.edu>
2023-09-15 20:50:36 -0400

    fix(python): better scheduling of black-macchiato write

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,4 +1,4 @@
-vim.keymap.set('n', "<A-f>", function()
+require("custom-utils").keymap('n', "<A-f>", function()
 	local params = {
 		command = 'pyright.organizeimports',
 		arguments = { vim.uri_from_bufnr(0) },
@@ -8,24 +8,25 @@ vim.keymap.set('n', "<A-f>", function()
 	local line_num = vim.api.nvim_win_get_cursor(0)[1]
 	local _pre = vim.api.nvim_buf_get_lines(0, 0, -1, false)
 	local fn = vim.api.nvim_buf_get_name(0)
-	vim.fn.jobstart("cat " .. fn .. " | black-macchiato",
+	vim.schedule(function()
+		vim.fn.jobstart("cat " .. fn .. " | black-macchiato",
 		{
 			stdout_buffered = true,
 			stderr_buffered = true,
 			on_exit = function(_, code, _)
 				if code ~= 0 and vim.api.nvim_buf_get_name(0) == fn then -- ERROR, reset lines
-					vim.api.nvim_buf_set_lines(0, 0, -1, false, _pre)
+					vim.schedule(function() vim.api.nvim_buf_set_lines(0, 0, -1, false, _pre) end)
 				end
 				vim.cmd(":" .. line_num)
 			end,
 			on_stdout = function(_, data)
 				if data and vim.api.nvim_buf_get_name(0) == fn then
 					table.remove(data, #data) -- black-macchiato adds an extra newline for some reason
-					vim.api.nvim_buf_set_lines(0, 0, -1, false, data)
+					vim.schedule(function() vim.api.nvim_buf_set_lines(0, 0, -1, false, data) end)
 				end
 			end,
-		}
-	)
+		})
+	end)
 end)
 vim.bo.softtabstop = 8
 vim.bo.expandtab = true
